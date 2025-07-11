d2d7685428f85ec47e2bfc497d68316fa8c10659
Muse Semu <smuse1756@gmail.com>
2025-07-11 10:30:00 +0300

    Back entry point for the on the script

--- crm/settings.py
+++ crm/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRONJOBS = [
+    ('*/5 * * * *', 'crm.cron.log_crm_heartbeat'),
     ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
 
